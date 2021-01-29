530a752256cff019ea38e2e015f9a0fd801a6ecb
jingwenbo <jingwenbo@aiyunxiao.com>
2021-01-29 18:59:02 +0800

    AQSDemo

---
+++ SpringMVC/springmvc-01-servlet/src/main/java/AQSDemo.java
@@ -0,0 +1,7 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class AQSDemo {
+    public static void main(String[] args) {
+        ReentrantLock reentrantLock = new ReentrantLock();
+    }
+}
